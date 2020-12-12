23d48e4b567734e809d12b7bb398f436a2249aae
Mary Jimenez <maryjios>
2020-12-11 20:20:01 -0500

    Agregadno archivo de reporte de ventas

--- inicio.php
+++ inicio.php
@@ -181,17 +181,11 @@ include_once "header.php" ?>
           </div>
           <!-- Card -->
         </div>
-        <div class="col-md-5 grid-margin stretch-card">
-          <div class="card">
-            <div class="card-body">
-              <p class="card-title">Total sales</p>
-              <h1>$ 28835</h1>
-              <h4>Gross sales over the years</h4>
-              <p class="text-muted">Today, many people rely on computers to do homework, work, and create or store useful information. Therefore, it is important </p>
-              <div id="total-sales-chart-legend"></div>
-            </div>
-            <canvas id="total-sales-chart"></canvas>
-          </div>
+
+  <?php } else {
+
+  header("Location: login.php");
+} ?>          </div>
         </div>
       </div>
     </div>
